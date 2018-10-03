df05c37792317e73737315ef0e865f9ea499061a
SultanaK <kr.shampa@yahoo.com>
2018-10-03 17:45:28 -0500

    Adding google Map

--- assets/JavaScript/JavaScript.js
+++ assets/JavaScript/JavaScript.js
@@ -7,3 +7,33 @@ $.ajax({
 }).then(function(res) {
   console.log(res);
 });
+
+/*Adding Google Map*/
+
+function initMap() {
+
+    // load the map
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 38, lng: -95},
+      zoom: 5,
+      styles: mapStyle
+    });
+
+    var mapStyle = [{
+      'featureType': 'all',
+      'elementType': 'all',
+      'stylers': [{'visibility': 'off'}]
+    }, {
+      'featureType': 'landscape',
+      'elementType': 'geometry',
+      'stylers': [{'visibility': 'on'}, {'color': '#fcfcfc'}]
+    }, {
+      'featureType': 'water',
+      'elementType': 'labels',
+      'stylers': [{'visibility': 'off'}]
+    }, {
+      'featureType': 'water',
+      'elementType': 'geometry',
+      'stylers': [{'visibility': 'on'}, {'hue': '#5f94ff'}, {'lightness': 60}]
+    }];
+  }
